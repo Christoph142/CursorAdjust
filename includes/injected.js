47dc2577fa01f0f51dae5277d66539f920a27276
Christoph <Christoph142@myopera.com>
2013-02-23 11:57:11 +0100

    encapsulate CursorAdjust

--- includes/injected.js
+++ includes/injected.js
@@ -1,13 +1,15 @@
-window.addEventListener("DOMContentLoaded", function(){
-	var style = document.createElement("style");
-	style.setAttribute("type","text/css");
-	style.innerHTML = "body{ cursor:default; } input[type=text],[type=password],[type=email]{ cursor:text; }";
-	try{ document.getElementsByTagName("head")[0].appendChild(style); }
-	catch(e){
-		try{
-			var head = document.createElement("head");
-			head.appendChild(style);
-			document.body.appendChild(head);
-		}catch(e){ opera.postError("CursorAdjust: style-insertion failed"); }
-	}
-}, false);
+(function(){
+	window.addEventListener("load", function(){
+		var style = document.createElement("style");
+		style.setAttribute("type","text/css");
+		style.innerHTML = "body{ cursor:default; } input[type=text],[type=password],[type=email]{ cursor:text; }";
+		try{ document.getElementsByTagName("head")[0].appendChild(style); }
+		catch(e){
+			try{
+				var head = document.createElement("head");
+				head.appendChild(style);
+				document.body.appendChild(head);
+			}catch(e){ opera.postError("CursorAdjust: style-insertion failed"); }
+		}
+	}, false);
+})();
