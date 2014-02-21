9b7c3d28820d0a2747dba8797bb0b917eb558a71
Christoph142 <christoph142@gmx.com>
2014-02-21 12:10:20 +0100

    fix / polish contenteditables

--- oex/includes/CursorAdjust.js
+++ oex/includes/CursorAdjust.js
@@ -2,13 +2,13 @@
 	window.addEventListener("load", function(){
 		var style = document.createElement("style");
 		style.setAttribute("type","text/css");
-		style.innerHTML = "body{ cursor:default; }";
+		style.innerHTML = "body{ cursor:default; }\n[contenteditable]{ cursor:text; }";
 		try{ document.getElementsByTagName("head")[0].appendChild(style); }
 		catch(e){
 			try{
 				var head = document.createElement("head");
 				head.appendChild(style);
-				document.body.appendChild(head);
+				document.documentElement.appendChild(head);
 			}catch(e){ console.log("CursorAdjust: style-insertion failed"); }
 		}
 	}, false);
